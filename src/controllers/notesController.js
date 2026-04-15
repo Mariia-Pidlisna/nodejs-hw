96bee18b48cb606961b2370da91efcf11404a624
Mariia Pidlisna <mariia.pidlisna@gmail.com>
2026-04-15 11:19:58 +0300

    fix one note

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,32 +2,23 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const { noteId } = req.params;
-  const note = await Note.findOne({ _id: noteId, userId: req.user._id });
-
-  if (!note) {
-    throw createHttpError(404, 'Note not found');
-  }
-
-  const page = Number(req.query.page) || 1;
-  const perPage = Number(req.query.perPage) || 10;
-  const { search, tag } = req.query;
+   const { page = 1, perPage = 10, search, tag } = req.query;
 
   const skip = (page - 1) * perPage;
 
- const filter = { userId: req.user._id };
+ const notesQuery = Note.find({ userId: req.user._id });
 
    if (search) {
-    filter.$text = { $search: search };
+     notesQuery.where({ $text: { $search: search } });
    }
 
    if (tag) {
-     filter.tag = tag;
+     notesQuery.where('tag').equals(tag);
   }
 
   const [totalNotes, notes] = await Promise.all([
-    Note.countDocuments(filter),
-    Note.find(filter).skip(skip).limit(perPage),
+    notesQuery.clone().countDocuments(),
+    notesQuery.skip(skip).limit(perPage),
   ]);
 
   const totalPages = Math.ceil(totalNotes / perPage);
@@ -38,7 +29,6 @@ export const getAllNotes = async (req, res) => {
     totalNotes,
     totalPages,
     notes,
-    note,
   });
 };
 
